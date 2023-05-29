05eb8b2f022d9db55d32186eb9c2d6d268ab5b45
raghad-wasef-qedan <raghadqedan@yahoo.com>
2023-05-29 06:47:37 +0300

    Update Animals App

--- AnimalsApp/app/src/main/java/com/example/animalsapp/MainActivity.kt
+++ AnimalsApp/app/src/main/java/com/example/animalsapp/MainActivity.kt
@@ -1,39 +1,47 @@
 package com.example.animalsapp
 
+import android.content.Context
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.AdapterView
 import android.widget.GridView
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.animalsapp.adapter.AnimalCardAdapter
 import com.example.animalsapp.data.DataSource
+
 import kotlin.math.log
 
 
-class MainActivity : AppCompatActivity() , AdapterView.OnItemClickListener{
-    lateinit var gridView: GridView
+class MainActivity : AppCompatActivity() {
+    lateinit var recyclerView: RecyclerView
     val data=DataSource
+    lateinit var image:ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-          gridView=findViewById(R.id.animalGridLayout)
-        val myAdapter=AnimalCardAdapter(this,data)
-        gridView.adapter=myAdapter
-        gridView.setOnItemClickListener(this)
+          recyclerView=findViewById(R.id.animalRecyclerView)
+        recyclerView.layoutManager=GridLayoutManager(this,3)
+        val image:ImageView=findViewById(R.id.animalImage)
+        val myAdapter=AnimalCardAdapter(this,data,image)
+        recyclerView.adapter=myAdapter
+        }
 
 
-    }
 
-    override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
 
-        val image=findViewById<ImageView>(R.id.animalImage)
-        val item = data.data[position]
-        image.setBackgroundResource(item.imagePath)
-        val mediaPlayer = MediaPlayer.create( this, R.raw.assets_audios_cat)
-        mediaPlayer.start()
-    }
+
+
+
+
+
 }
+
+
--- AnimalsApp/app/src/main/java/com/example/animalsapp/adapter/AnimalCardAdapter.kt
+++ AnimalsApp/app/src/main/java/com/example/animalsapp/adapter/AnimalCardAdapter.kt
@@ -2,52 +2,89 @@ package com.example.animalsapp.adapter
 
 import android.content.Context
 import android.graphics.Color.rgb
+import android.graphics.drawable.Drawable
 import android.media.MediaPlayer
+import android.os.CountDownTimer
+import android.util.Log
 
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
 import com.example.animalsapp.R
 import com.example.animalsapp.data.DataSource
 import com.google.android.material.button.MaterialButton
 
 
 class AnimalCardAdapter(
-     var context:Context?,
-     val data:DataSource
+     var context:Context,
+     val data:DataSource,
+     private val animalsImage:ImageView
 
-):BaseAdapter(){
+):RecyclerView.Adapter<AnimalCardAdapter.AnimalViewHolder>() {
+    var mediaPlayer: MediaPlayer? = null
+    private var timer: CountDownTimer? = null
 
-
-
-    override fun getCount(): Int {
-       return data.data.size
+    class AnimalViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        var button = view.findViewById<MaterialButton>(R.id.button)
     }
 
-    override fun getItem(position: Int): Any {
-      return  data.data.get(position)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AnimalViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.button_item, parent, false)
+        return AnimalViewHolder(view)
     }
 
-    override fun getItemId(position: Int): Long {
-         return 0
-    }
+    override fun onBindViewHolder(holder: AnimalViewHolder, position: Int) {
 
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-       val view=LayoutInflater.from(context ).inflate(R.layout.button_item,parent,false)
-        var  b =view?.findViewById<MaterialButton>(R.id.button)
-        b?.setText(data.data.get(position).name)
-        if(position==0 || position%2==0){
-            if (b != null) {
-                b.setBackgroundColor(rgb(192, 127, 0))
+        holder.button.setText(data.data.get(position).name)
+        if (position == 0 || position % 2 == 0) {
+            if (holder.button != null) {
+                holder.button.setBackgroundColor(rgb(192, 127, 0))
             }
         }
+           holder.button.setOnClickListener {
+            val item = data.data[position]
+            animalsImage?.setImageResource(item.imagePath)
+            //to prevent the animals sound overlapping
+            mediaPlayer?.release()
+            mediaPlayer = null
+            timer?.cancel()
 
-        return view
+            // Create a new CountDownTimer with a delay of 1 second
+            timer = object : CountDownTimer(1000, 100000) {
+                override fun onTick(millisUntilFinished: Long) {
+                    // Not used
+                }
+                override fun onFinish() {
+                    // Create and start a new MediaPlayer instance
+                    mediaPlayer = MediaPlayer.create(context, item.soundPath)
+                    mediaPlayer?.start()
+                }
+            }
+            // Start the timer
+            timer?.start()
+            val mediaPlayer = MediaPlayer.create(context, item.soundPath)
+            mediaPlayer.start()
+        }
+    }
+    override fun onViewRecycled(holder: AnimalViewHolder) {
+        super.onViewRecycled(holder)
+        // Release MediaPlayer instance when the view is recycled
+        mediaPlayer?.release()
+
+        // Cancel the timer if running
+        timer?.cancel()
     }
 
-
+    override fun getItemCount(): Int {
+        return data.data.size
+    }
 }
+
+
+
+
 
 
